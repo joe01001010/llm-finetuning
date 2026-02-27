8caf97093dcf9dd54711c39bf80589bd35ca2095
Joe Weibel <jweibel@uccs.edu>
2026-02-26 17:58:26 -0700

    Added the data pull functions

---
+++ python/full-pipeline.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# Main entry point for the python library
+
+import requests, kagglehub
+from pathlib import Path
+
+
+def pull_data(dataset_name, output_directory):
+    """
+    This function takes two arguments
+    One argument is the output directory to download the dataset to
+    This function takes the path for a dataset as an argument
+    This function will pull the dataset from kaggle and download it locally
+    This function will return the path to the dataset
+    """
+    output_directory.mkdir(parents=True, exist_ok=True)
+    return kagglehub.dataset_download(dataset_name, output_dir=str(output_directory), force_download=True,)
+
+
+def main():
+    """
+    This program is designed to pull data and save it locally from the internet
+    This program will clean and format the data for the AI agent to receive
+    """
+    weather_data_path = pull_data("ananthr1/weather-prediction", Path(__file__).resolve().parent.parent / "data" / "weather-prediction")
+    print(weather_data_path)
+
+
+if __name__ == '__main__':
+    main()
