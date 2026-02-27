7d6f8dd7dec6b8888dd33300b156f7156e17102b
joeweibel77@gmail.com <jweibel@uccs.edu>
2026-02-26 19:59:38 -0700

    Fixes based on pylint

--- python/full_pipeline.py
+++ python/full_pipeline.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # Main entry point for the python library
 
-import requests, kagglehub
 from pathlib import Path
+import kagglehub
+
 
 
 def pull_data(dataset_name, output_directory):
@@ -14,7 +15,9 @@ def pull_data(dataset_name, output_directory):
     This function will return the path to the dataset
     """
     output_directory.mkdir(parents=True, exist_ok=True)
-    return kagglehub.dataset_download(dataset_name, output_dir=str(output_directory), force_download=True,)
+    return kagglehub.dataset_download(dataset_name,
+        output_dir=str(output_directory),
+        force_download=True,)
 
 
 def main():
@@ -22,7 +25,8 @@ def main():
     This program is designed to pull data and save it locally from the internet
     This program will clean and format the data for the AI agent to receive
     """
-    weather_data_path = pull_data("ananthr1/weather-prediction", Path(__file__).resolve().parent.parent / "data" / "weather-prediction")
+    weather_data_path = pull_data("ananthr1/weather-prediction",
+        Path(__file__).resolve().parent.parent / "data" / "weather-prediction")
     print(weather_data_path)
 
 
