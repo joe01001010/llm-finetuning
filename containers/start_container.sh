66784d8f893805b83be04508f9b8a4c13506ecf3
Joe Weibel <155695530+joe01001010@users.noreply.github.com>
2026-03-01 13:04:45 -0700

    Created a python script to pull a model locally from hugging face (#12)
    * Created a python script to pull a model locally from hugging face
    
    * Made changes based on pylint
    
    * Made changes based on pylint
    
    * Made changes based on pylint

--- containers/start_container.sh
+++ containers/start_container.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+cd ~/git/llm-finetuning
 docker rm -f llm-finetuning
-docker run -it -v $(pwd):/llm-finetuning --name=llm-finetuning --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 43e6802c38a5
+docker run -it -v ~/.cache:/root/.cache -v /tmp/:/local-containers -v $(pwd):/llm-finetuning --name=llm-finetuning --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 43e6802c38a5
---
+++ python/pull_model.py
@@ -0,0 +1,33 @@
+"""
+pull_model.py
+This program is designed to pull QWEN 7B
+"""
+#!/usr/bin/env python
+
+
+import os
+from huggingface_hub import snapshot_download
+
+
+MODEL_ID = "Qwen/Qwen2-7B-Instruct"
+MODEL_DIR = f"/local-containers/{MODEL_ID.rsplit('/', maxsplit=1)[-1]}"
+
+
+def main():
+    """
+    This function will check if the model dir exists locally
+    If the model directory doesnt exist it will download the model
+    """
+    if os.path.exists(MODEL_DIR):
+        print(f"Model {MODEL_ID} is already downloaded")
+    else:
+        path = snapshot_download(
+            repo_id = MODEL_ID,
+            local_dir = MODEL_DIR,
+            local_dir_use_symlinks = False,
+        )
+        print(f"Downloaded model {MODEL_ID}: {path}")
+
+
+if __name__ == '__main__':
+    main()
