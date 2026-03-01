2d7afc5578169500099e683637dda689f2ac3246
Joe Weibel <jweibel@uccs.edu>
2026-03-01 13:01:36 -0700

    Made changes based on pylint

--- python/pull_model.py
+++ python/pull_model.py
@@ -10,18 +10,22 @@ from huggingface_hub import snapshot_download
 
 
 MODEL_ID = "Qwen/Qwen2-7B-Instruct"
-MODEL_DIR = f"/local-containers/{MODEL_ID.split('/')[-1]}"
+MODEL_DIR = f"/local-containers/{MODEL_ID.rsplit('/', maxsplit=1)[-1]}"
 
 
 def main():
-  if os.path.exists(MODEL_DIR):
-    print(f"Model {MODEL_ID} is already downloaded")
-  else:
-    path = snapshot_download(
-      repo_id = MODEL_ID,
-      local_dir = MODEL_DIR,
-      local_dir_use_symlinks = False,
-    )
+    """
+    This function will check if the model dir exists locally
+    If the model directory doesnt exist it will download the model
+    """
+    if os.path.exists(MODEL_DIR):
+        print(f"Model {MODEL_ID} is already downloaded")
+    else:
+        path = snapshot_download(
+            repo_id = MODEL_ID,
+            local_dir = MODEL_DIR,
+            local_dir_use_symlinks = False,
+        )
 
 
 if __name__ == '__main__':
