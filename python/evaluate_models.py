e8c0199bac93a11e980068a4ffc6b837f05d052d
Joe Weibel <jweibel@uccs.edu>
2026-03-03 19:19:37 -0700

    Stopping point. Need to create the evaluate program

--- python/evaluate_models.py
+++ python/evaluate_models.py
@@ -9,12 +9,14 @@ import math
 from typing import Dict, Any, List, Tuple
 import torch
 from datasets import load_dataset
-from transformers import AutoTokenizerm, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM
 from peft import PeftModel
 
 
 MODEL_PATH = "/local-containers/Qwen2-7B-Instruct"
 ADAPTER_PATH = "./lora-weights"
+DATA_PATH = "./data/seattle_weather_chat.json"
+REQUIRED_KEYS = ["temp_max", "temp_min", "precipitation", "wind", "weather"]
 
 
 def main():
